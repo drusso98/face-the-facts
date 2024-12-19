36288d05f879bd2310319445a9a4f89495f46913
drusso98 <ru.daniel98@gmail.com>
2024-12-19 01:21:16 +0100

    fixed path

--- generate.sh
+++ generate.sh
@@ -9,5 +9,5 @@ while IFS=, read -r model_id model_type quantization; do
     python generation/generation_code_chunks.py -model_id $model_id -model_type $model_type -quantization $quantization
     python generation/generation_code_articles.py -model_id $model_id -model_type $model_type -quantization $quantization
 done < "$input_file"
-python evaluation/automatic_eval.py -input_dir results/test_set/chunks -output_dir evaluation/results/automatic_eval/chunks -context_len 10
-python evaluation/automatic_eval.py -input_dir results/test_set/articles -output_dir evaluation/results/automatic_eval/articles -context_len 1
+python automatic_eval.py -input_dir results/test_set/chunks -output_dir evaluation/results/automatic_eval/chunks -context_len 10
+python automatic_eval.py -input_dir results/test_set/articles -output_dir evaluation/results/automatic_eval/articles -context_len 1
